4944b2d800542aa093eb4a1efd117a59633d47b0
Hamish Duff <hduff@esri.com>
2023-06-30 10:44:44 +0100

    Implemented GC collect button

--- MauiMemoryLeakTestApp/SecondaryPage.xaml.cs
+++ MauiMemoryLeakTestApp/SecondaryPage.xaml.cs
@@ -2,13 +2,23 @@ namespace MauiMemoryLeakTestApp;
 
 public partial class SecondaryPage : ContentPage
 {
-	public SecondaryPage()
-	{
-		InitializeComponent();
-	}
+    public SecondaryPage()
+    {
+        InitializeComponent();
+    }
 
     private async void OpenTertiaryPageButton_Clicked(object sender, EventArgs e)
     {
-		await Navigation.PushAsync(new TertiaryPage());
+        await Navigation.PushAsync(new TertiaryPage());
+    }
+
+    protected override void OnNavigatedTo(NavigatedToEventArgs args)
+    {
+        base.OnNavigatedTo(args);
+    }
+
+    private void GCCollectButton_Clicked(object sender, EventArgs e)
+    {
+        GC.Collect();
     }
 }
